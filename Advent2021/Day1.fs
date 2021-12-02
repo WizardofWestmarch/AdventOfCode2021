e78711c16884bed5d0ee662420bde0141b4394af
Patrick Sullivan <wizardofwestmarch@gmail.com>
2021-12-01 17:21:44 -0700

    Initial commit with day 1 of Avent 2021.

---
+++ Advent2021/Day1.fs
@@ -0,0 +1,26 @@
+﻿module Advent2021.Day1
+
+let rec foldfunc (currentlist: List<string>) lastvalue counter =
+    match currentlist with
+    | head :: rest when int head > lastvalue -> foldfunc rest (int head) (counter+1)
+    | head :: rest -> foldfunc rest (int head) counter
+    | [ head ] when (int head) > lastvalue -> counter + 1
+    | [ _ ] -> counter
+    | _ -> counter
+
+
+//NOTE: I start at -1 because the first pass always adds as I am lazy.
+let rec foldfunc2 (currentlist: List<string>) lastsum counter =
+    match currentlist with
+    | head1 :: head2 :: head3 :: rest when (int head1) + (int head2) + (int head3) > lastsum -> foldfunc2 currentlist.Tail ((int head1) + (int head2) + (int head3)) (counter + 1)
+    | head1 :: head2 :: head3 :: rest -> foldfunc2 currentlist.Tail ((int head1) + (int head2) + (int head3)) counter
+    | head1 :: head2 :: [head3] when (int head1) + (int head2) + (int head3) > lastsum -> counter
+    | _ :: _ :: [_] -> counter
+    | _ -> counter
+
+let RunDay1 () =
+    let lines = System.IO.File.ReadLines(@"C:\users\dorma\Documents\AdventDay1Input.txt")
+    let result = foldfunc (Seq.toList lines) 0 -1
+    printfn "Day 1 Part 1: %i" result
+    let result2 = foldfunc2 (Seq.toList lines) 0 -1
+    printfn "Day 1 Part 2: %i" result2
---
+++ Advent2021/Program.fs
@@ -0,0 +1,7 @@
+
+
+// For more information see https://aka.ms/fsharp-console-apps
+
+//NOTE: I start at -1 because the first pass always adds as I am lazy.
+open Advent2021.Day1
+RunDay1 ()
